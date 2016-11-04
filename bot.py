1f7e2bd0eb19f844cd5a0bf7f852e2c34b84e8bc
Emily Tagtow <emily.tagtow@utexas.edu>
2016-11-04 14:17:25 -0400

    botgit add .

---
+++ bot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy
+import time
+import sys
+import os
+from random import randint
+
+argfile = str(sys.argv[1])
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_KEY = os.environ['ACCESS_KEY']
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+if __name__ == "__main__":
+
+    while True:
+        with open(argfile) as f:
+            lines = f.readlines()
+            rand_line_num = randint(0, len(lines) - 1)
+            line = lines[rand_line_num]
+            api.update_status(line)
+            time.sleep(3600)  # Tweet every 60 minutes
